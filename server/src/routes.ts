ae304e0d3f89e7f5590f7c52f79d50888ed7077c
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-01-24 01:49:55 -0300

    Detail of day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,4 +27,47 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+    const parseDate = dayjs(date).startOf("day");
+    const weekDay = parseDate.get("day");
+
+    console.log(date, weekDay);
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        habitWeekDays: {
+          some: {
+            weed_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.day_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
 }
