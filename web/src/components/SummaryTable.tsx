1628dd82cc8f19e35a155d2a422c3e2797e6e57d
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-01-24 23:21:30 -0300

    Create daily habits chart

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,11 @@
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-column gap-3">
+        <div className="text-zinc-400 text-xl h-10 w-10 font-bold justify-center items-center">
+          D
+        </div>
+      </div>
+    </div>
+  );
+}
