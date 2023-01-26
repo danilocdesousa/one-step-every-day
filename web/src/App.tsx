eb931381d7296bd478faafe0d6f5cc7778e1acf4
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-01-25 22:12:55 -0300

    Update daily habits chart

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
-// import { Habit } from "./components/Habit";
 
 export function App() {
   return (
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,10 +1,25 @@
+import { HabitDay } from "./HabitDay";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
 export function SummaryTable() {
   return (
     <div className="w-full flex">
-      <div className="grid grid-rows-7 grid-flow-column gap-3">
-        <div className="text-zinc-400 text-xl h-10 w-10 font-bold justify-center items-center">
-          D
-        </div>
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold justify-center items-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        <HabitDay />
       </div>
     </div>
   );
