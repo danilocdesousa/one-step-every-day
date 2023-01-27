e31b2274f0b01c361beca923cef0636a0d5ee048
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-01-27 01:05:42 -0300

    Number of fill days

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -3,6 +3,8 @@ import { HabitDay } from "./HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const summaryDates = generateStartOfYearDates();
+const minSummaryDateSize = 18 * 7; // 18 weeks
+const NumberOfFillsDays = minSummaryDateSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -21,9 +23,19 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
+        {summaryDates.map((date) => {
           return <HabitDay key={date.toString()} />;
         })}
+
+        {NumberOfFillsDays > 0 &&
+          Array.from({ length: NumberOfFillsDays }).map((_, i) => {
+            return (
+              <div
+                key={i}
+                className="w-10 h-10 bg-zinc-900 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
