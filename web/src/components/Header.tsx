9d2b8edfb0b6423c02840ad6b34e7060bd831a2c
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-01-30 23:50:27 -0300

    Create a habit-creation modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,16 +1,30 @@
 import { Plus } from "phosphor-react";
 import logoImage from "../assets/habits_logo.svg";
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
+  
+
+
   return (
     <div className="w-full max-w-3xl mx-auto flex justify-between items-center">
       <img src={logoImage} alt="Habits logo" />
-      <button
-        type="button"
-        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-400"
-      >
-        <Plus size={20} className="text-violet-500" /> Novo hábito
-      </button>
+      
+      <Dialog.Root>
+        <Dialog.Trigger
+          type="button"
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-400"
+        >
+          <Plus size={20} className="text-violet-500" /> Novo hábito
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0"/>
+          <Dialog.Content>
+            Modal content
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
