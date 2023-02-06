cca8fe19038a7c48c445276542442b83288130e8
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-02-05 22:17:14 -0300

    Create the checkboxes

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,7 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import * as Popover from "@radix-ui/react-popover";
 import clsx from "clsx";
+import { Check } from "phosphor-react";
 import { ProgressBar } from "./ProgressBar";
 
 interface HabitDayProps {
@@ -28,12 +30,24 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
       />
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400">quartaa-feira</span>
+          <span className="font-semibold text-zinc-400">quarta-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
             01/02
           </span>
           <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
             <ProgressBar progress={completedPercentage} />
+          </div>
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through  group-data-[state=checked]:text-zinc-400">
+                Meditar
+              </span>
+            </Checkbox.Root>
           </div>
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- web/src/components/newHabitForm.tsx
+++ web/src/components/newHabitForm.tsx
@@ -1,4 +1,15 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function NewHabitForm() {
   return (
@@ -6,16 +17,35 @@ export function NewHabitForm() {
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual o seu comprometimento?
       </label>
+
       <input
         type={"text"}
         id={"title"}
-        placeholder={"Ex.: Exercicíos, beber água, etc."}
+        placeholder={"Ex.: exercícios, beber água, etc."}
         autoFocus
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
       />
+
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map((weekDay) => {
+          return (
+            <Checkbox.Root key={weekDay} className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="text-white leading-tight">{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
+
       <button
         type="submit"
         className="mt-6 flex justify-center items-center rounded-lg p-4 gap-3 font-semibold bg-green-600 hover:bg-green-500"
