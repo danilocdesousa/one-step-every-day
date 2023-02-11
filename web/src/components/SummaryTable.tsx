5fc059f038a0d4826836138976881787bf0b89a7
Danilo S <45634291+danilocdesousa@users.noreply.github.com>
2023-02-10 22:28:36 -0300

    Search the API summary

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import { generateStartOfYearDates } from "../utils/generate-start-of-year dates";
 import { HabitDay } from "./HabitDay";
+import { api } from "../lib/axios";
+import dayjs from "dayjs";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const summaryDates = generateStartOfYearDates();
 const minSummaryDateSize = 18 * 7; // 18 weeks
-const NumberOfFillsDays = minSummaryDateSize - summaryDates.length;
+const numberOfFillsDays = minSummaryDateSize - summaryDates.length;
+
+type Summary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[];
 
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    api.get("/summary").then((response) => {
+      setSummary(response.data);
+    });
+  }, []);
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -24,17 +42,22 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(date).isSame(day.date, "day");
+          });
+
           return (
             <HabitDay
               key={date.toString()}
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           );
         })}
 
-        {NumberOfFillsDays > 0 &&
-          Array.from({ length: NumberOfFillsDays }).map((_, i) => {
+        {numberOfFillsDays > 0 &&
+          Array.from({ length: numberOfFillsDays }).map((_, i) => {
             return (
               <div
                 key={i}
